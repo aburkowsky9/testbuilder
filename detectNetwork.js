9d1aa3e2e02434dc2102e4c3f95a51d46d594f23
Alex Burkowsky <aburkowsky9@gmail.com>
2018-08-22 07:37:10 -0700

    Step 3: Add conditionals for Discover and Maestro and add tests

--- detectNetwork.js
+++ detectNetwork.js
@@ -5,12 +5,15 @@
 // How can you tell one card network from another? Easy! 
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
+  // Note: `cardNumber` will always be a string
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
+  // Step 1!---------------------------------------------------
   var cardNumLength = cardNumber.length;
   var firstTwoInts = cardNumber.slice(0, 2);
+  var firstThreeInts = cardNumber.slice(0, 3);
+  var firstFourInts = cardNumber.slice(0, 4);
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   if (cardNumLength === 14 && (firstTwoInts === '38' || firstTwoInts === '39')) {
   	return 'Diner\'s Club';
@@ -20,6 +23,7 @@ var detectNetwork = function(cardNumber) {
   	return 'American Express';
   }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  // Step 2!-----------------------------------------------------
   // Visa always has a prefix of 4 and a length of 13, 16, or 19.
   else if ((cardNumLength === 13 || cardNumLength === 16 || cardNumLength === 19) && cardNumber[0] === '4') {
   	return 'Visa';
@@ -28,6 +32,18 @@ var detectNetwork = function(cardNumber) {
   else if (cardNumLength === 16 && (firstTwoInts === '51' || firstTwoInts === '52' || firstTwoInts === '53' || firstTwoInts === '54' || firstTwoInts === '55')) {
   	return 'MasterCard';
   }
+  // Step 3!--------------------------------------------------------
+  // Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
+  else if ((cardNumLength === 16 || cardNumLength ===19) && (firstTwoInts === '65' || (Number(firstThreeInts) >= 644 && Number(firstThreeInts) <= 649) || firstFourInts === '6011')) {
+  	return 'Discover';
+  }
+  //Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+  else if ((cardNumLength >= 12 && cardNumLength <= 19) && (firstFourInts === '5018' || firstFourInts === '5020' || firstFourInts === '5038' || firstFourInts === '6304')) {
+  	return 'Maestro';
+  }
 };
+
+
+
 
 
